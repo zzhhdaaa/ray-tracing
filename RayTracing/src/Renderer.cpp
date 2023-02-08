0a844f33507c151df49e1372e6aa9ce1a3e07c81
zzhhdaaa <zzhhdaaa@g.ucla.edu>
2023-02-08 10:42:15 -0800

    calculate the lighting
    - Create a basic lighting

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -24,6 +24,7 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 
 void Renderer::Render()
 {
+	float aspectRatio = (float)m_FinalImage->GetWidth() / (float)m_FinalImage->GetHeight();
 	//render every pixel
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
@@ -31,6 +32,7 @@ void Renderer::Render()
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f; //remap to [-1, 1]
+			coord.x *= aspectRatio;
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
 		}
 	}
@@ -40,20 +42,21 @@ void Renderer::Render()
 
 uint32_t Renderer::PerPixel(glm::vec2 coord)
 {
-	uint8_t r = (uint8_t)((coord.y/2.0f + 0.5f) * 255.0f);
-	uint8_t g = (uint8_t)((coord.y/2.0f + 0.5f) * 255.0f);
-
-	glm::vec3 rayOrigin(0.0f, 0.0f, 3.0f);
+	// ray
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
-	rayDirection = glm::normalize(rayDirection);
+	rayDirection = glm::normalize(rayDirection); //don't really have to normalize
 
-	glm::vec3 sphereOrigin(0.5f, 1.5f, 0.0f);
+	// sphere
+	glm::vec3 sphereOrigin(0.0f, 0.0f, 0.0f);
 	float radius = 0.5f;
-	// rayDirection = glm::normalize(rayDirection);
+
+	// light
+	glm::vec3 lightDir(-1.0f, -1.0f, 1.0f);
+	lightDir = glm::normalize(lightDir);
 
 	// ray = p + td
 	// p = ray origin, d = ray direction, o = circle origin, r = radius, t = hit distance
-	// (dx^2 + dy^2)t^2 + 2(pxdx + pydy)t + (px^2 + py^2 - r^2) = 0
 	// (dx^2 + dy^2)t^2 + 2(pxdx + pydy - (dxox + dyoy))t + (px^2 + py^2 + ox^2 + oy^2 - 2(pxox + pyoy) - r^2) = 0
 	// delta = b^2 - 4ac
 
@@ -65,13 +68,37 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 
 	if (delta >= 0.0f)
 	{
-		float t1 = (-b + pow(delta, 0.5f)) / (2 * a);
-		float t2 = (-b - pow(delta, 0.5f)) / (2 * a);
+		float t[] = {
+			(-b + pow(delta, 0.5f)) / (2.0f * a),
+			(-b - pow(delta, 0.5f)) / (2.0f * a)
+		};
 
-		uint8_t b = (uint8_t)(255.0f - (t2 / 2.0f) * 255.0f);
+		if (t[0] - t[1] <= 0.15f)
+		{
+			return 0xffffffff;
+		}
 
-		return 0xff000000 | b << 16;
+		for (int i = 0; i < 2; i++)
+		{
+			glm::vec3 hitPosition = rayOrigin + rayDirection * t[i];
+			glm::vec3 hitNormal = glm::normalize(hitPosition - sphereOrigin);
+
+			//For two vectors a, b
+			//the dot product, dot(a, b) = |a||b|cos¦È
+			//which represents the similarity between two vectors' directions.
+			//In this case, the more the hitNormal faces where the light comes from, 
+			//the multiplier gets larger.
+			float lightMultiplier = std::max(glm::dot(hitNormal, -lightDir), 0.0f);
+
+			uint8_t r = (uint8_t)((hitNormal.x / 2.0f + 0.5f) * lightMultiplier * 255.0f);
+			uint8_t g = (uint8_t)((hitNormal.y / 2.0f + 0.5f) * lightMultiplier * 255.0f);
+			uint8_t b = (uint8_t)((hitNormal.z / 2.0f + 0.5f) * lightMultiplier * 255.0f);
+			return 0xff000000 | b << 16 | g << 8 | r;
+		}
 	}
+
+	uint8_t r = (uint8_t)((coord.y / 2.0f + 0.5f) * 255.0f);
+	uint8_t g = (uint8_t)((coord.y / 2.0f + 0.5f) * 255.0f);
 
 	return 0xff000000 | (g << 8) | r;
 }
